ef042aa365b8828926ce41fd71106790b74c4e19
Marek Counts <countsmarek@gmail.com>
2023-06-23 14:50:57 -0400

    setup gh-action pages deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+const nextConfig = {
+    images: {
+        loader: 'akamai',
+        path:'',
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+}
 
 module.exports = nextConfig
